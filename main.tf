da4592910311ad7a653b8ce45a1a7ac1f04bd868
digua <lteng0212@163.com>
2022-10-24 19:52:50 +0800

    atlantis help

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_resource_group" "rg" {
   location = "westus2"
    tags     = {
         "Environment" = "Terraform Getting Started"
-        "Team"        = "DevOps12345s"
+        "Team"        = "DevOps12345s2"
     }
 }
 
