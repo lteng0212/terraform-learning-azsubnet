1d805faa6c3e6d2d243b78362edb21017ebc6068
huifeng <huifeng@pinduoduo.com>
2023-01-06 15:03:56 +0800

    addJenkins

--- main.tf
+++ main.tf
@@ -1,15 +1,8 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-
-  required_version = ">= 1.1.0"
+output "jenkins_terraform" {
+  value = " test running Terraform from Jenkins"
 }
 
+/*
 provider "azurerm" {
   features {}
 }
@@ -29,3 +22,4 @@ resource "azurerm_virtual_network" "vnet" {
   location            = "westus2"
   resource_group_name = azurerm_resource_group.rg.name
 }
+*/
